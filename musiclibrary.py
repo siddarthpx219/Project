f9af547bad99183539d3af1192e58d1bd428f3a1
Siddarth <131779523+siddarthpx219@users.noreply.github.com>
2024-07-14 19:09:01 +0530

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,19 @@
+music = {
+    "skyfall" : "https://music.youtube.com/watch?v=LJzp_mDxaT0&si=gaiPHG-45IW9eVfz",
+    "Challa" : "https://music.youtube.com/watch?v=dTd4itJegNo&si=CzHC4bwcDBCAgVfd",
+    "Somethingjustlikethis" : "https://music.youtube.com/watch?v=owTWCbq_nSk&si=0nNIhcrytIMnU50j",
+    "Shboom" : "https://music.youtube.com/watch?v=owTWCbq_nSk&si=0nNIhcrytIMnU50j",
+    "HeatWaves" : "https://music.youtube.com/watch?v=XDjB9E3YtUE&si=_8Slwf9MPYTXoiXr",
+    "Uptownfunk" : "https://music.youtube.com/watch?v=tYvFa2ARD24&si=5oPwS05qXBeDwV-U",
+    "Rasputin" : "https://music.youtube.com/watch?v=Y2_hKPShcpY&si=xP8MGZcWil12lfjw",
+    "HeyJude" : "https://music.youtube.com/watch?v=mQER0A0ej0M&si=XwwqRzWGhhh5Huzp",
+    "KillerQueen" : "https://music.youtube.com/watch?v=bR-gZQLO26w&si=kTchBoQoUYjAp7FS",
+    "WearetheChampions" : "https://music.youtube.com/watch?v=Gk781Z0ErmA&si=wmOjtk7E0InpVfFB",
+    "drumsofliberation": "https://music.youtube.com/watch?v=ZNl4nelnboY&si=KKyrq05cSCSrPIL1",
+    "Illuminati" : "https://music.youtube.com/watch?v=S1jo8K4kDJc&si=yIcm8B4cVwxTsUov",
+    "MadhuraiVeeran" : "https://youtu.be/I49FOqU7qFY?si=QuJIriugsR_7-4XQ",
+    "gunshot" : "https://youtu.be/F1IAjoAp2fM?si=MGlyqi2fQVbDqqR3",
+    "Tuhaikaha" : "https://music.youtube.com/watch?v=Y5m3FAxatX4&si=Bup7OxDsx_HxIL7f",
+    "Aasakooda" : "https://youtu.be/a3Ue-LN5B9U?si=FdwkDzAiBe2hUVkS",
+    "gaalivaluga" : "https://music.youtube.com/watch?v=d9ul2C4f3CU&si=TWdKkJyO6DOnFuhW"
+}
